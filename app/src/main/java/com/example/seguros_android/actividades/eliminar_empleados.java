2ff0e401b78501fd867e03592ca9eea2f82427e3
alpa-k <alejandrolopeztecno@gmail.com>
2020-03-16 18:48:57 +0100

    act pantalla quitar cliente

---
+++ app/src/main/java/com/example/seguros_android/actividades/eliminar_empleados.java
@@ -0,0 +1,18 @@
+package com.example.seguros_android.actividades;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.seguros_android.R;
+
+public class eliminar_empleados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config_seguro);
+    }
+}
